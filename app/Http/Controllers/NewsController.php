921372bd4e0c2c6271dbd04e155973d67d0ea012
ashan <ashan.lin@gmail.com>
2019-06-05 10:32:38 +0800

    2019/06/05 blog  Eloquent
    Signed-off-by: ashan <ashan.lin@gmail.com>

---
+++ app/Http/Controllers/NewsController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use News;
+use DB; 
+
+class NewsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        DB::insert('insert into `users` (`name`, `email`, `password`) values (?, ?, ?)', ['陳潔熙','jc861217@gmail.com','cc861217']);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show()
+    {
+        $users = DB::table('users')->get();
+        dd($users);
+        // foreach($users as $user){
+        //     return $user->name;
+        // }
+
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request)
+    {
+        //
+        $updated = DB::update('update users set name = "林金山" where id=1');
+        return $updated;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy()
+    {
+        //
+        $deleted = DB::delete('delete from users where id = 5');
+        return $deleted;
+    }
+}
